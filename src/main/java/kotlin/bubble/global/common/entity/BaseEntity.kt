ce40e632c3f13f62bb884201f69cd60e032590f3
jiyeon0926 <yeoni9094@naver.com>
2025-05-12 17:10:24 +0900

    feat: BaseEntity
    - createAt
    - updateAt

---
+++ src/main/java/kotlin/bubble/global/common/entity/BaseEntity.kt
@@ -0,0 +1,18 @@
+package kotlin.bubble.global.common.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
+
+@MappedSuperclass
+abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    var createAt: LocalDateTime = LocalDateTime.now()
+
+    @LastModifiedDate
+    var updateAt: LocalDateTime = LocalDateTime.now()
+}
